1f409e0362f32f65d3dc54c3999dd6b10b9d2711
NoahLei <net111730@gmail.com>
2022-10-16 22:59:10 -0400

    started TwoNumber

---
+++ Arrays/Easy/TwoNumberSum.java
@@ -0,0 +1,37 @@
+/**
+ * Two Number Sum
+ * Write a function that takes in a non-empty array of distinct integers and an integer representing a target
+ * sum. If any two numbers in the input array sum up to the target sum, the function should return them in
+ * an array, in any order. If no two numbers sum up to the target sum, the function should return an empty
+ * array.
+ * Note that the target sum has to be obtained by summing two different integers in the array; you can't add
+ * a single integer to itself in order to obtain the target sum.
+ * You can assume that there will be at most one pair of numbers summing up to the target sum.
+ * <p>
+ * Sample Input
+ * array = [3, 5, -4, 8, 11, 1, -1, 6]
+ * targetSum = 10
+ * <p>
+ * Sample Output
+ * [-1, 11] // the numbers could be in reverse order
+ *
+ */
+
+
+public class TwoNumberSum {
+
+    public static void main(String[] args){
+
+    }
+
+    // Solution 1
+    public static int[] twoNumberSum_SolutionOne(int[] array, int targetSum){
+
+
+
+        return new int[0];
+
+    }
+
+
+}
